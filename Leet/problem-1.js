3ec36a4bf0689f05d9fe3645b6e8ac43d260f33a
Sudharsanhari06 <sudharsanhari2006@gmail.com>
2025-01-26 14:13:30 +0530

    convert the lowercase

---
+++ Leet/problem-1.js
@@ -0,0 +1,14 @@
+let s="SUDHARsan";
+let str="";
+for(let i=0;i<=s.length-1;i++){
+    if(s[i]>='A' && s[i]<='Z'){
+       let b=s[i].charCodeAt(0)+32;
+    //   console.log(b)
+       let z=String.fromCharCode(b)
+      str+=z;
+    }
+    else{
+        str+=s[i];
+    }   
+}
+console.log(str);
